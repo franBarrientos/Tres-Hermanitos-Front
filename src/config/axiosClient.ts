e41e98d0077f81ddc135407a55a2abad1e1ea6db
Franco Barrientos <francobarrientos56@gmail.com>
2023-07-14 08:11:08 -0300

    cambio la base url a string para netlify

--- src/config/axiosClient.ts
+++ src/config/axiosClient.ts
@@ -1,5 +1,5 @@
 import axios from "axios";
-const apiUrl: string = import.meta.env.VITE_BASE_URL;
+const apiUrl: string = "https://ecommerce-backv3-production.up.railway.app/api";
 const apiClient = axios.create({
   baseURL: apiUrl,
   timeout: 5000,
